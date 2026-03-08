476d54bf4fe0fe159d2a94a7b72f8dba0aa9bad6
Fureza Muqaddas <furezamuqaddas@gmail.com>
2026-03-08 14:25:30 -0400

    data transformation

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from src.mlproject.components import data_transformation
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.utils import read_sql_data
+from src.mlproject.components.data_transformation import DataTransformationConfig
+from src.mlproject.utils import save_object
 import sys
 
 
@@ -8,8 +13,16 @@ if __name__ == "__main__":
     logging.info("the execution has started")
 
     try:
-        obj= DataIngestion()
-        obj.initiate_data_ingestion()
+        #obj= DataIngestion()
+        #obj.initiate_data_ingestion()
+        data_ingestion = DataIngestion()
+        data_transformation = DataTransformation()
+        
+        train_path = r'artifacts\train.csv'
+        test_path = r'artifacts\test.csv'
+        data_transformation.initiate_data_transformation(train_path, test_path)
+
+
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,8 @@ import pymysql  # resonsible for connecting the db
 from src.mlproject.exception import  CustomException
 from src.mlproject.logger import logging
 import pandas as pd
+import pickle 
+import numpy as np
  
 
 load_dotenv()
@@ -35,3 +37,21 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex, sys)
+    
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)       
+         
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+        
